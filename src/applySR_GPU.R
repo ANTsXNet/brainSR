48d28342c1c4b8c2f8e74d6eb18ab520f5e664df
stnava <stnava@gmail.com>
2019-07-02 12:25:09 -0400

    WIP: back to generalizable

--- src/applySR_GPU.R
+++ src/applySR_GPU.R
@@ -19,7 +19,8 @@ for ( x in 1:length( inputFileName ) ) {
   print( paste( "Apply SR to:", inputFileName[ x ]  ) )
   img = antsImageRead( inputFileName[ x ] )
   mysr = applySuperResolutionModelPatch( img, mdl,
-    targetRange = c( 127.5, -127.5 ), lowResolutionPatchSize=24 )
+    targetRange = c( 127.5, -127.5 ), lowResolutionPatchSize=128,
+    strideLength=41 )
   dir.create( file.path( './', 'results_GPU'), showWarnings = FALSE)
   antsImageWrite( mysr, paste0( './results_GPU/', outfn, "_SR.nii.gz" ) )
 }

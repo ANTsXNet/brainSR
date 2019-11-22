313137026d6b5d5b6ac56308117a2bf1eead2f5b
stnava <stnava@gmail.com>
2019-11-22 08:02:00 -0500

    ENH: better aug

--- src/applySR.R
+++ src/applySR.R
@@ -26,8 +26,21 @@ for ( x in 1:length( inputFileName ) ) {
      strides = rep( 2, 3 ),
      numberOfLossFunctions = 1 )
   load_model_weights_hdf5( mdl, modelFile )
+  augger <- function( image, trans, interpolation = 'linear' ) {
+    moveX1 = createAntsrTransform(dimension=3, type="Euler3DTransform", translation=trans )
+    shift1 = applyAntsrTransform(transform=moveX1, data=image, reference=image, interpolation=interpolation )
+    srout = applySuperResolutionModel( shift1, mdl, targetRange = c( 127.5, -127.5) )
+    return( applyAntsrTransform( transform = invertAntsrTransform( moveX1 ), data=srout, reference=srout, interpolation=interpolation  ) )
+    }
   print('Begin')
-  mysr = applySuperResolutionModel( img, mdl, targetRange = c( 127.5, -127.5) )
+  iaug = list()
+  for ( k in 1:10 )
+    iaug[[k]] = augger( img, antsGetSpacing( img ) * rnorm(3) )
+  wt = 0.2
+  mysr = antsAverageImages( iaug, normalize=FALSE )
+  mysr2 = mysr * (1-wt) + iMath( mysr, "Sharpen" ) * wt
   dir.create( file.path( './', 'results'), showWarnings = FALSE)
   antsImageWrite( mysr, paste0( './results/', outfn, "_SR.nii.gz" ) )
+  antsImageWrite( mysr2, paste0( './results/', outfn, "_SR2.nii.gz" ) )
+  # antsImageWrite( mysrf, paste0( './results/', outfn, "_SRf.nii.gz" ) )
 }
